b08bbb71980d507ac7e649bd7758c4cd44eda94d
Andrii <loborchuka@ukr.net>
2020-06-16 21:21:23 +0300

    GG

---
+++ index.php
@@ -0,0 +1,14 @@
+<div align="center">
+<iframe width="500" height="500"  src="https://www.youtube.com/embed/D6qyI8kHUkw?list=PL0lO_mIqDDFXm69bqj5JTCS1XGTNkhTch" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<title>GG</title>
+<?php
+  echo "<br />"."<div align=\"center\">fgdgdgkjjhkhjkrtyrty</div>.<br />";
+  echo $_SERVER['REMOTE_ADDR'];
+  $mess='Bello';
+  $to ='loborchuka@gmail.com';
+  $back='Main';
+  mail($to,$back,$mess);
+  phpinfo();
+?>
+  <input type="text"/>
